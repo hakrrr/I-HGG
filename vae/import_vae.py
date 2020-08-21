9d9585ffb872787058ce0f816150c815c079b8ae
hakrrr <ryojamesli@gmail.com>
2020-08-21 17:38:21 +0200

    manipulate.py

--- vae/import_vae.py
+++ vae/import_vae.py
@@ -7,7 +7,7 @@ vae_fetch_push = VAE_PUSH.load_Vae(path='data/FetchPush/vae_model_goal')
 vae_egg = VAE_EGG.load_Vae(path='data/HandManipulate/vae_model_egg')
 vae_hand_reach = VAE_HAND_REACH.load_Vae(path='data/HandManipulate/vae_model_reach')
 
-goal_set_push = np.load('data/FetchPush/goal_set.npy')
+goal_set_fetch_push = np.load('data/FetchPush/goal_set.npy')
 goal_set_egg = np.load('data/HandManipulate/egg_goal_set.npy')
 goal_set_reach = np.load('data/HandManipulate/reach_goal_set.npy')
 
